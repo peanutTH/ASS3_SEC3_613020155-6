85fb7b4882696fcaa2767dd2425612ed525b3d24
phuwanaich <70085407+phuwanaich@users.noreply.github.com>
2020-08-23 21:10:58 +0700

    ส่งงาน Assignment3

---
+++ app/src/main/java/com/myweb/ass3_sec3_613020155_6/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.myweb.ass3_sec3_613020155_6
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
